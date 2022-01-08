5fb1abebfa65be3dd57a9d3d9e4be1112c3afb9f
meghna <meghnabarn2001@gmail.com>
2022-01-08 11:17:50 +0530

    day4 done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:first_app/pages/home_page.dart';
 import 'package:first_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,18 +12,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int days = 30;
-    String s = "meghna";
+    // int days = 30;
+    // String s = "meghna";
     return MaterialApp(
       // home: HomePgae(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(
+          primarySwatch: Colors.red,
+          fontFamily: GoogleFonts.lato().fontFamily,
+          primaryTextTheme: GoogleFonts.latoTextTheme()),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/home",
-      routes: {
-        "/": (context) => LoginPgae(),
-        "/home": (context) => HomePgae()
-      },
+      initialRoute: "/",
+      routes: {"/": (context) => LoginPgae(), "/home": (context) => HomePgae()},
     );
   }
 }
